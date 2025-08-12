ebff0db9eb4281d1006f22713201db724d4f1da2
alice <aliciabasilo.ab@gmail.com>
2025-08-12 04:52:07 +0000

    test

--- scraper.py
+++ scraper.py
@@ -93,6 +93,7 @@ def run_ai_summary(start_url):
     time.sleep(3)
 
     print("🔎 Extracting news headlines...")
+    print("🔎 Hello!!!...")
     articles = bot.extract_news()
 
     # 📨 SQS client
